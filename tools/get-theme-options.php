3a6e07d0c854e2c29fe97aa4c92252f53b058503
schneidan <dan@scrollwright.com>
2015-02-06 18:08:32 -0700

    Trying to fix some PHP notices so I can find errors

--- tools/get-theme-options.php
+++ tools/get-theme-options.php
@@ -2,7 +2,7 @@
 //allows the theme to get info from the theme options page
 global $options;
 foreach ($options as $value) {
-    if ( isset( $value['id'] ) {
+    if ( isset( $value['id'] ) ) {
     	if (get_option( $value['id'] ) === FALSE) {
     		$$value['id'] = $value['std'];
     	} else {
